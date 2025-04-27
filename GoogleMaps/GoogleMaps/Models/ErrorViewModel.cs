2cd11f29d3a62caf84a33437c1cf7db68c0cfa6c
JuanJ189 <juanjosetitoescobar18@gmail.com>
2025-04-27 11:45:39 -0400

    Confirmacion

--- GoogleMaps/GoogleMaps/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GoogleMaps.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
